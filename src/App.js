c764bcbc3b2f18e25412c80380771b6e3ffd58ae
Chong Wen Hao <58220142+8kdesign@users.noreply.github.com>
2022-05-16 11:51:30 +0800

    Reset

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { Container } from "react-bootstrap";
-
-import PatientSearchScreen from "./pages/PatientSearchScreen";
-import PatientConditionScreen from "./pages/PatientConditionScreen";
 
 function App() {
 	return (
-		<Container>
-			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<PatientSearchScreen />} />
-					<Route
-						path="/condition"
-						element={<PatientConditionScreen />}
-					/>
-					<Route path="*" element={<Navigate to="/" replace />} />
-				</Routes>
-			</BrowserRouter>
-		</Container>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<div />} />
+				<Route path="*" element={<Navigate to="/" replace />} />
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
