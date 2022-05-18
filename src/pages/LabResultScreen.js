629adaf313cb3737918a7d4c15b36d8fd88d2676
Chong Wen Hao <58220142+8kdesign@users.noreply.github.com>
2022-05-18 09:04:55 +0800

    Add Nav & Screens

---
+++ src/pages/LabResultScreen.js
@@ -0,0 +1,3 @@
+export default function LabResultScreen() {
+	return <></>;
+}
--- src/pages/SynopsisScreen.js
+++ src/pages/SynopsisScreen.js
@@ -1,12 +1,25 @@
 import { useState } from "react";
+import AdditionalNoteScreen from "./AdditionalNoteScreen";
+import AppointmentScreen from "./AppointmentScreen";
+import CaregivingScreen from "./CaregivingScreen";
+import HomeMonitoringScreen from "./HomeMonitoringScreen";
+import LabResultScreen from "./LabResultScreen";
+import MedicationScreen from "./MedicationScreen";
+import ToDoListScreen from "./ToDoListScreen";
 
 export default function SynopsisScreen() {
 	const [screenIndex, setScreenIndex] = useState(0);
 
 	return (
 		<div className="SynopsisPage">
-			<div className="hide-if-large TopNav"></div>
-			<div className="hide-if-small SideNav"></div>
+			<TopNavBar
+				screenIndex={screenIndex}
+				setScreenIndex={setScreenIndex}
+			/>
+			<SideNavBar
+				screenIndex={screenIndex}
+				setScreenIndex={setScreenIndex}
+			/>
 			<div className="Content">
 				<Content screenIndex={screenIndex} />
 			</div>
@@ -17,18 +30,55 @@ export default function SynopsisScreen() {
 function Content({ screenIndex }) {
 	switch (screenIndex) {
 		case 0:
-			return <></>; // To-Do List
+			return <ToDoListScreen />; // To-Do List
 		case 1:
-			return <></>; // Home Monitoring
+			return <HomeMonitoringScreen />; // Home Monitoring
 		case 2:
-			return <></>; // Medication
+			return <MedicationScreen />; // Medication
 		case 3:
-			return <></>; // Lab Results
+			return <LabResultScreen />; // Lab Results
 		case 4:
-			return <></>; // Upcoming Appointments
+			return <AppointmentScreen />; // Upcoming Appointments
 		case 5:
-			return <></>; // Caregiving
+			return <CaregivingScreen />; // Caregiving
 		default:
-			return <></>; // Additional Notes
+			return <AdditionalNoteScreen />; // Additional Notes
 	}
+}
+
+var screenNames = [
+	"To-Do",
+	"Home Monitoring",
+	"Medication",
+	"Lab Results",
+	"Upcoming Appointments",
+	"Caregiving",
+	"Doctor's Comments",
+];
+
+function TopNavBar({ screenIndex, setScreenIndex }) {
+	return <div className="hide-if-large TopNav"></div>;
+}
+
+function SideNavBar({ screenIndex, setScreenIndex }) {
+	var toggles = [];
+	for (let i = 0; i < screenNames.length; i++) {
+		if (i == screenIndex) {
+			toggles.push(
+				<p className="SideNavText active">{screenNames[i]}</p>
+			);
+		} else {
+			toggles.push(
+				<p
+					className="SideNavText"
+					onClick={() => {
+						setScreenIndex(i);
+					}}
+				>
+					{screenNames[i]}
+				</p>
+			);
+		}
+	}
+	return <div className="hide-if-small SideNav">{toggles}</div>;
 }
