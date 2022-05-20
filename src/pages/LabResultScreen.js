36442014d5f62e87a9b0524ec73017f7ed9c5fa2
Ananya Gupta <ananyagupta99200@gmail.com>
2022-05-21 02:16:33 +0800

    next

--- src/pages/LabResultScreen.js
+++ src/pages/LabResultScreen.js
@@ -1,20 +1,42 @@
 import React from 'react';
-import Data from "./data.json";
+import JsonData from './data.json';
+const url = 'https://www.healthhub.sg/HealtheServices'
 
-export default function LabResultScreen() {
-	return (
-		<div className="LabResultScreen">
-			<h1> Access Lab Reports and Analysis below</h1>
-			<div className="posts">
-				{ Data.map(post => {
-					return(
-						<div key={post.id } className="post">
-							<h3>{ post.title } </h3>
-							<p>{ post.content }</p>
-						</div>
-					)
-				}) }
+function LabResultScreen(){
+    const DisplayData=JsonData.map(
+        (info)=>{
+            return(
+                <tr>
+                    <td>{info.title}</td>
+                    <td>{info.content}</td>
+                </tr>
+            )
+        }
+    )
+
+    return(
+        <div>
+            <center><h3>Access Lab Reports and doctor's suggestions</h3></center>
+			<div 
+      			className='card-wrapper'
+      			onClick={() => window.open(url, '_blank')}  
+			>
+				<span><center><u>Click here for lab reports</u></center></span>
 			</div>
-		</div>
-	);
-}
+            <div>
+			<center><h4>Doctor's Analysis</h4></center>
+            <table class="table table-striped">
+                <tbody>
+
+                    {DisplayData}
+                    
+                </tbody>
+            </table>
+            </div>
+        </div>
+    )
+ }
+ 
+
+export default LabResultScreen;
+
