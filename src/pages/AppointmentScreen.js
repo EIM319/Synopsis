b9fd7f637938e4975460cbb2c03ec868352dafae
Ananya Gupta <ananyagupta99200@gmail.com>
2022-05-30 21:31:56 +0800

    new change

--- src/pages/AppointmentScreen.js
+++ src/pages/AppointmentScreen.js
@@ -3,38 +3,42 @@ import { Container } from "react-bootstrap";
 import JsonData from "../mockdata/calendar_events.json";
 
 function AppointmentScreen() {
-	const DisplayData = JsonData.map((info) => {
-<<<<<<< Updated upstream
-		return (
-			<tr>
-				<td style={{ fontWeight: 500, fontSize: 17 }}>{info.title}</td>
-				<td>{info.value}</td>
-			</tr>
-		);
-=======
-		info.forEach((item) => {
-			return (
-				<tr key={item}>
-					<td style={{ fontWeight: 500, fontSize: 17 }}>{info.date}</td>
+	const DisplayData=JsonData.map(
+		(info)=>{
+			return(
+				<tr>
+					<td>{info.date + " " +info.month + " " + info.year}</td>
+					<td>{info.day}</td>
+					<td>{info.hour+":"+info.min+" " +info.period}</td>
 					<td>{info.name}</td>
 					<td>{info.location}</td>
 					<td>{info.doctor}</td>
 					<td>{info.time}</td>
 				</tr>
-			);
-		});
->>>>>>> Stashed changes
-	});
-	return (
-		<Container style={{ padding: 20 }}>
+			)
+		}
+	)
+	return(
+		<Container style={{ padding:20 }}>
 			<p className="sectionHeader">Upcoming Appointments</p>
 			<div>
 				<table class="table table-striped">
+					<thead>
+						<tr>
+						<th>Date</th>
+						<th>Day</th>
+						<th>Time</th>
+						<th>Purpose</th>
+						<th>Location</th>
+						<th>Consulting Doctor</th>
+						<th>Waiting Time</th>
+						</tr>
+					</thead>
 					<tbody>{DisplayData}</tbody>
 				</table>
 			</div>
 		</Container>
-	);
+	)
 }
 
 export default AppointmentScreen;
