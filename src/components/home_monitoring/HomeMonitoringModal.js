7d3dbc1f24db5e76938a6570139f26730b726ab5
Chong Wen Hao <58220142+8kdesign@users.noreply.github.com>
2022-05-31 07:59:12 +0800

    Add close button

--- src/components/home_monitoring/HomeMonitoringModal.js
+++ src/components/home_monitoring/HomeMonitoringModal.js
@@ -6,26 +6,6 @@ export function HomeMonitoringModal({ openModal, setOpenModal, monitor }) {
 	const components = [];
 	monitor.content.forEach((item) => {
 		switch (item.type) {
-			case "purpose":
-				components.push(
-					<p
-						style={{
-							fontSize: 23,
-							fontWeight: 500,
-							paddingTop: 10,
-						}}
-					>
-						{item.content}
-					</p>
-				);
-				break;
-			case "subheader":
-				components.push(
-					<p style={{ fontSize: 17, paddingBottom: 20 }}>
-						{item.content}
-					</p>
-				);
-				break;
 			case "image":
 				components.push(
 					<Image
@@ -43,7 +23,14 @@ export function HomeMonitoringModal({ openModal, setOpenModal, monitor }) {
 				break;
 			case "section":
 				components.push(
-					<p style={{ fontWeight: 700, fontSize: 17 }}>
+					<p
+						style={{
+							fontWeight: 700,
+							fontSize: 17,
+							paddingTop: 20,
+							paddingBottom: 10,
+						}}
+					>
 						{item.content}
 					</p>
 				);
@@ -64,6 +51,9 @@ export function HomeMonitoringModal({ openModal, setOpenModal, monitor }) {
 	});
 	return (
 		<Modal show={openModal} onHide={() => setOpenModal(false)} centered>
+			<Modal.Header closeButton>
+				<Modal.Title>{monitor.name}</Modal.Title>
+			</Modal.Header>
 			<Row style={{ padding: 20 }}>{components}</Row>
 		</Modal>
 	);
