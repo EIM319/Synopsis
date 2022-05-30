3abfe3ee8f94a8c019b06e75e95fb45f7f21ef77
Chong Wen Hao <58220142+8kdesign@users.noreply.github.com>
2022-05-30 17:15:06 +0800

    make it better

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import SynopsisScreen from "./pages/SynopsisScreen";
 
 function App() {
+	Notification.requestPermission().then((value) => {
+		if (value === "granted") {
+			prepareNotification();
+		}
+	});
+
 	return (
 		<BrowserRouter>
 			<Routes>
@@ -15,3 +21,25 @@ function App() {
 }
 
 export default App;
+
+async function prepareNotification() {
+	navigator.serviceWorker.register("service-worker.js");
+	navigator.serviceWorker.ready.then(function (serviceWorkerRegistration) {
+		var options = {
+			userVisibleOnly: true,
+			applicationServerKey:
+				"BIH3Nifzw8KH2lSGTK-aAZ0HNnop-kUuWSRttfVVRGm1Cr5yU61XwW9bZ67Asantr8zJ-0h_klYFxKQwW7VAvVM",
+		};
+		serviceWorkerRegistration.pushManager.subscribe(options).then(
+			function (pushSubscription) {
+				console.log(JSON.stringify(pushSubscription.endpoint));
+				// The push subscription details needed by the application
+				// server are now available, and can be sent to it using,
+				// for example, an XMLHttpRequest.
+			},
+			function (error) {
+				console.log(error);
+			}
+		);
+	});
+}
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import * as ServiceWorkerRegistration from "./serviceWorkerRegistration";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -11,12 +10,6 @@ root.render(
 		<App />
 	</React.StrictMode>
 );
-
-Notification.requestPermission().then((value) => {
-	if (value === "granted") {
-		ServiceWorkerRegistration.register();
-	}
-});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
