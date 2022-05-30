caebcd76b49f1cf313b55d81df54e767854a428c
Chong Wen Hao <58220142+8kdesign@users.noreply.github.com>
2022-05-30 19:16:15 +0800

    Fix notification

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,11 +1,3 @@
-// // This allows the web app to trigger skipWaiting via
-// // registration.waiting.postMessage({type: 'SKIP_WAITING'})
-// self.addEventListener("message", (event) => {
-// 	if (event.data && event.data.type === "SKIP_WAITING") {
-// 		self.skipWaiting();
-// 	}
-// });
-
 self.addEventListener("push", (event) => {
-	new Notification("Working", { body: "Text Body" });
+	self.registration.showNotification(event.data.text());
 });
--- push.js
+++ push.js
@@ -6,14 +6,20 @@ const id = {
 	privateKey: "lu6MtsdYNfOaTVUEQWKVw6rt6CEGdEefvgQje-2991k",
 };
 
+push.setVapidDetails(
+	"https://eim319synopsis.herokuapp.com/",
+	id.publicKey,
+	id.privateKey
+);
+
 const sub = {
 	endpoint:
-		"https://fcm.googleapis.com/fcm/send/e_vb8c7LSX0:AP…uwsIkoZ1QZMqFURMwmeDD_gpO1pZ5n_H81o8hTlXae8M4WTZ1",
+		"https://fcm.googleapis.com/fcm/send/drlXREUjSvY:APA91bGZV4ISgvj6CbIzbtLTngO7LpdvMioTsENh_Uvpd04A_jBAoFySx0KQSlIQ0zhtciQH19bxazaLKVbB3TahN6zvNjbSU4UFvy7oFgSQ2q8pjXGy8FoMOL1FXzp0tGut_-dkHmwt",
 	expirationTime: null,
 	keys: {
-		p256dh: "BIH3Nifzw8KH2lSGTK-aAZ0HNnop-kUuWSRttfVVRGm1Cr5yU61XwW9bZ67Asantr8zJ-0h_klYFxKQwW7VAvVM",
-		auth: "",
+		p256dh: "BCQHDTv9HaNo1sqDrSkPxSdBUe8kgr5KKWzwN6bRKoMPkaI3DPpmDnjndvlrbxSn4whOfliCKMU9FGC0ri0pGiU",
+		auth: "vJAFESSULaE2-lFK0m2E1Q",
 	},
 };
 
-push.sendNotification(sub, "Message");
+push.sendNotification(sub, "Message goes here");
