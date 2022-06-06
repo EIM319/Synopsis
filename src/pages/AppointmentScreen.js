5e16a62073877fd05e7b7bce3e1b4ec8c9eb6987
Chong Wen Hao <58220142+8kdesign@users.noreply.github.com>
2022-06-06 09:14:43 +0800

    Update AppointmentScreen.js

--- src/pages/AppointmentScreen.js
+++ src/pages/AppointmentScreen.js
@@ -9,22 +9,7 @@ function AppointmentScreen() {
 		const bDate = b.date + b.month * 40 + b.year * 400;
 		return aDate - bDate;
 	});
-	const DisplayData = events.map((info) => {
-		return (
-			<tr>
-				<td>
-					{info.date + " " + monthNames[info.month] + " " + info.year}
-					<br />
-					{"(" + info.day + ")"}
-				</td>
-				<td>{info.hour + ":" + info.min + " " + info.period}</td>
-				<td>{info.name}</td>
-				<td>{info.location}</td>
-				<td>{info.doctor}</td>
-				<td>{info.time}</td>
-			</tr>
-		);
-	});
+
 	return (
 		<Container style={{ padding: "20, 10, 20, 10" }}>
 			<div style={{ maxWidth: 1000, paddingBottom: 50 }}>
@@ -39,19 +24,9 @@ function AppointmentScreen() {
 				</p>
 				<br />
 				<div>
-					<table className="table table-striped appointmentTable">
-						<thead>
-							<tr>
-								<th>Date</th>
-								<th>Time</th>
-								<th>Purpose</th>
-								<th>Location</th>
-								<th>Doctor</th>
-								<th>Waiting Time</th>
-							</tr>
-						</thead>
-						<tbody>{DisplayData}</tbody>
-					</table>
+					<div className="appointmentTable">
+						<AppointmentsTable events={events} />
+					</div>
 					<div className="appointmentCards">
 						<AppointmentCards events={events} />
 					</div>
@@ -63,37 +38,94 @@ function AppointmentScreen() {
 
 export default AppointmentScreen;
 
+function AppointmentsTable({ events }) {
+	const today = new Date();
+	const DisplayData = events
+		.filter((event) => {
+			if (event.year < today.getFullYear()) return false;
+			if (event.month < today.getMonth()) return false;
+			if (event.date < today.getDate()) return false;
+			return true;
+		})
+		.map((info) => {
+			return (
+				<tr>
+					<td>
+						{info.date +
+							" " +
+							monthNames[info.month] +
+							" " +
+							info.year}
+						<br />
+						{"(" + info.day + ")"}
+					</td>
+					<td>{info.hour + ":" + info.min + " " + info.period}</td>
+					<td>{info.name}</td>
+					<td>{info.location}</td>
+					<td>{info.doctor}</td>
+					<td>{info.time}</td>
+				</tr>
+			);
+		});
+
+	return (
+		<table className="table table-striped">
+			<thead>
+				<tr>
+					<th>Date</th>
+					<th>Time</th>
+					<th>Purpose</th>
+					<th>Location</th>
+					<th>Doctor</th>
+					<th>Waiting Time</th>
+				</tr>
+			</thead>
+			<tbody>{DisplayData}</tbody>
+		</table>
+	);
+}
+
 function AppointmentCards({ events }) {
 	const array = [];
-	events.forEach((event) => {
-		array.push(
-			<div className="itemCard" style={{ padding: 20 }}>
-				<p style={{ fontSize: 17, fontWeight: 500 }}>{event.name}</p>
-				<p style={{ fontSize: 15 }}>
-					{event.date +
-						" " +
-						monthNames[event.month] +
-						" " +
-						event.year}
-					{" (" + event.day + ")"}{" "}
-					{event.hour + ":" + event.min + " " + event.period}
-				</p>
-				<div className="line-horizontal" />
+	const today = new Date();
+	events
+		.filter((event) => {
+			if (event.year < today.getFullYear()) return false;
+			if (event.month < today.getMonth()) return false;
+			if (event.date < today.getDate()) return false;
+			return true;
+		})
+		.forEach((event) => {
+			array.push(
+				<div className="itemCard" style={{ padding: 20 }}>
+					<p style={{ fontSize: 17, fontWeight: 500 }}>
+						{event.name}
+					</p>
+					<p style={{ fontSize: 15 }}>
+						{event.date +
+							" " +
+							monthNames[event.month] +
+							" " +
+							event.year}
+						{" (" + event.day + ")"}{" "}
+						{event.hour + ":" + event.min + " " + event.period}
+					</p>
+					<div className="line-horizontal" />
 
-				<p style={{ fontSize: 15 }}>
-					<b>Location: </b>
-					{event.location}
-				</p>
-				<p style={{ fontSize: 15 }}>
-					<b>Doctor: </b>
-					{event.doctor}
-				</p>
-				<p style={{ fontSize: 15 }}>
-					<b>Waiting Time: </b>
-					{event.time}
-				</p>
-			</div>
-		);
-	});
+					<p style={{ fontSize: 15 }}>
+						<b>Location: </b>
+						{event.location}
+					</p>
+					<p style={{ fontSize: 15 }}>
+						<b>Doctor: </b>
+						{event.doctor}
+					</p>
+					<p style={{ fontSize: 15 }}>
+						<b>Waiting Time: </b>
+						{event.time}
+					</p>
+				</div>
+			);
+		});
 	return array;
 }
