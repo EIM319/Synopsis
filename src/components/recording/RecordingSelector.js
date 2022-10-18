061066ac8c5292fcd2e8d4449240bc82ee134dec
Chong Wen Hao <58220142+8kdesign@users.noreply.github.com>
2022-10-18 16:54:44 +0800

    Add view recording screen

---
+++ src/components/recording/RecordingSelector.js
@@ -0,0 +1,53 @@
+export default function RecordingsSelector({
+	recordings,
+	setSelectedRecording,
+	selectedRecording,
+}) {
+	const items = [];
+	recordings.forEach((value, key) => {
+		if (selectedRecording !== undefined && key === selectedRecording[0]) {
+			items.push(
+				<p
+					className="toggle"
+					style={{
+						padding: "10px 20px",
+						background: "var(--accent)",
+						color: "white",
+						borderRadius: 5,
+						margin: 0,
+					}}
+				>
+					{key}
+				</p>
+			);
+		} else {
+			items.push(
+				<p
+					className="toggle"
+					style={{
+						padding: "10px 20px",
+						margin: 0,
+					}}
+					onClick={() => {
+						setSelectedRecording([key, value]);
+					}}
+				>
+					{key}
+				</p>
+			);
+		}
+	});
+
+	return (
+		<div
+			style={{
+				display: "flex",
+				flexDirection: "row",
+				width: "100%",
+				overflowX: "scroll",
+			}}
+		>
+			{items}
+		</div>
+	);
+}
